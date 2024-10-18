a7c1f75b55ea796179f87401e79059ef57a2b1f8
safidy3 <safidy.anyaina@gmail.com>
2024-10-18 10:31:53 +0300

    feat : split input with < and >

--- Safidy/ft_split_esc_2.c
+++ Safidy/ft_split_esc_2.c
@@ -14,6 +14,11 @@ static void	*ft_free_exit(char **tab)
 	return (NULL);
 }
 
+static int	is_special_char(char c)
+{
+	return (c == '<' || c == '>');
+}
+
 static char	*ft_escap_spliter(const char *s, char c)
 {
 	while (*s == c)
@@ -33,6 +38,21 @@ static char	*ft_escape_quote(const char *s)
 	return ((char *) s);
 }
 
+static const char *ft_escape_special_char(const char *s)
+{
+	if (is_special_char(*s))
+	{
+		s++;
+		if (is_special_char(*s))
+			s++;
+		while (*s == ' ')
+			s++;
+		while (*s && *s != ' ' && !is_special_char(*s))
+			s++;
+	}
+	return (s);
+}
+
 static size_t	ft_count_words(const char *s, char c)
 {
 	size_t	count;
@@ -41,19 +61,55 @@ static size_t	ft_count_words(const char *s, char c)
 	while (*s)
 	{
 		s = ft_escap_spliter(s, c);
-		if (*s && *s != c)
+		if (*s && !is_special_char(*s))
 		{
 			count++;
-			while (*s && *s != c)
+			while (*s && *s != c && !is_special_char(*s))
 			{
 				if (*s == '\'' || *s == '\"')
 					s = ft_escape_quote(s);
-				if (*s && *s != c)
+				if (*s && *s != c && !is_special_char(*s))
 					s++;
 			}
 		}
+		else if (*s && is_special_char(*s))
+		{
+			count++;
+			s = ft_escape_special_char(s);
+		}
 	}
 	return (count);
+}
+
+static void	skip_quote(const char *s, int *word_len, int *i)
+{
+	char quote;
+
+	if (s[*i] == '\'' || s[*i] == '"')
+	{
+		quote = s[(*i)++];
+		while (s[*i] && s[*i] != quote)
+			(*i)++;
+		if (s[*i])
+			(*i)++;
+		*word_len -= 2;
+	}
+}
+
+static void handle_special_char(const char *s, int *word_len, int *i)
+{
+	(*i)++;
+	if (is_special_char(s[*i]))
+		(*i)++;
+	while (s[*i] == ' ')
+		(*i)++;
+	while (s[*i] && s[*i] != ' ' && !is_special_char(s[*i]))
+	{
+		if (s[*i] == '\'' || s[*i] == '"')
+			skip_quote(s, word_len, i);
+		else if (s[*i])
+			(*i)++;
+	}
 }
 
 static size_t	ft_count_words_len(const char *s, char c)
@@ -64,20 +120,20 @@ static size_t	ft_count_words_len(const char *s, char c)
 
 	word_len = 0;
 	i = 0;
-	while (s[i] && s[i] != c)
+	if (s[i] && s[i] != c && !is_special_char(s[i]))
 	{
-		if (s[i] == '\'' || s[i] == '"')
+		while (s[i] && s[i] != c && !is_special_char(s[i]))
 		{
-			quote = s[i++];
-			while (s[i] && s[i] != quote)
+			if (s[i] == '\'' || s[i] == '"')
+				skip_quote(s, &word_len, &i);
+			else if (s[i] && s[i] != c && !is_special_char(s[i]))
 				i++;
-			if (s[i])
-				i++;
-			word_len -= 2;
+			else if (s[i] == c || is_special_char(s[i]))
+				break;
 		}
-		else if (s[i] && s[i] != c)
-			i++;
 	}
+	else if (is_special_char(s[i]))
+		handle_special_char(s, &word_len, &i);
 	word_len += i;
 	return (word_len);
 }
@@ -104,8 +160,8 @@ static char	*cpy_to_arr(char const *s, char *tab, int word_len)
 			tab[j++] = s[i++];
 	}
 	s += i;
-	if (*s)
-		s++;
+	// if (*s)
+	// 	s++;
 	return ((char *)s);
 }
 
@@ -121,6 +177,10 @@ char	**ft_split_esc_2(char const *s, char c)
 	tab = (char **)calloc(sizeof(char *), (words + 1));
 	if (!tab || words == 0)
 		return (NULL);
+	word_len = ft_count_words_len(s, c);
+	tab[i] = (char *)calloc(sizeof(char), (word_len + 1));
+	if (!tab[i])
+		return (ft_free_exit(tab));
 	while (++i < words)
 	{
 		s = ft_escap_spliter(s, c);
@@ -129,6 +189,7 @@ char	**ft_split_esc_2(char const *s, char c)
 		if (!tab[i])
 			return (ft_free_exit(tab));
 		s = cpy_to_arr(s, tab[i], word_len);
+		printf("tab[i] = %s : %zu\n", tab[i], word_len);
 	}
 	return (tab);
 }
