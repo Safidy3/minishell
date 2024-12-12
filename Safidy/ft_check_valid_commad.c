b6dec3a8faec237a647652cf25ac541ff0ecefec
Safidy3 <safidy.anyaina@gmail.com>
2024-12-12 11:07:40 +0300

    fix : validate command

--- Safidy/ft_check_valid_commad.c
+++ Safidy/ft_check_valid_commad.c
@@ -6,7 +6,7 @@
 /*   By: safandri <safandri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 11:47:46 by larakoto          #+#    #+#             */
-/*   Updated: 2024/12/10 10:43:43 by safandri         ###   ########.fr       */
+/*   Updated: 2024/12/12 10:55:59 by safandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,29 @@ int	is_symbol(char c)
 	return (0);
 }
 
+int	symbols_condition(char *command, int i)
+{
+	if (command[i] == '<' || command[i] == '>' || command[i] == '|'
+		|| command[i] == '\0')
+		return (-1);
+	return (0);
+}
+
+int ft_check_quote(char *command , char cote, int *i)
+{
+	if (command[*i] == cote)
+	{
+		(*i)++;
+		while (command[*i] && command[*i] != cote)
+			(*i)++;
+		if (command[*i] == '\0')
+			return(ft_putstr_fd("cote ts mihidy\n", 2), -1);
+		// if (command[*i + 1])
+		// 	(*i)++;
+	}
+	return(1);
+}
+
 int	check_symbol(char *command, char c, int *i)
 {
 	*i = *i + 1;
@@ -26,28 +49,84 @@ int	check_symbol(char *command, char c, int *i)
 	{
 		while (ft_isspace(command[*i]))
 			*i = *i + 1;
-		if (command[*i] == '<' || command[*i] == '>' || command[*i] == '|'
-			|| command[*i] == '\0')
+		if (symbols_condition(command, *i) == -1)
 			return (-1);
 	}
 	if (command[*i] == c)
 	{
 		*i = *i + 1;
-		if (command[*i] == '<' || command[*i] == '>' || command[*i] == '|'
-			|| command[*i] == '\0')
+		if (command[*i] == '<' || command[*i] == '>'
+			|| command[*i] == '|' || command[*i] == '\0')
 			return (-1);
 		if (ft_isspace(command[*i]))
 		{
 			while (ft_isspace(command[*i]))
 				*i = *i + 1;
-			if (command[*i] == '<' || command[*i] == '>' || command[*i] == '|'
-				|| command[*i] == '\0')
+			if (symbols_condition(command, *i) == -1)
 				return (-1);
 		}
 	}
 	if (is_symbol(command[*i]))
 		return (-1);
+	(*i)--;
 	return (0);
+}
+
+int	handle_pipe(int *flag_redirection, char *command, int *i)
+{
+	if (command[*i] == '|')
+	{
+		if (ft_isspace(command[*i]))
+		{
+			while (ft_isspace(command[*i]))
+				i++;
+			if (command[*i] == '|')
+				*flag_redirection = 1;
+		}
+		else if (command[*i + 1] == '|' || command[*i + 1] == '\0')
+			*flag_redirection = 1;
+	}
+	return (0);
+}
+
+int	handle_redirection(int *flag_redirection, char *command, int *i)
+{
+	if (command[*i] == '>' || command[*i] == '<')
+	{
+		if (check_symbol(command, command[*i], &(*i)) == -1)
+			*flag_redirection = 1;
+		if (!command[*i])
+			return (1);
+	}
+	return (0);
+}
+
+int	print_syntax_error(int *flag_redirection)
+{
+	if (*flag_redirection == 1)
+	{
+		ft_putstr_fd("bash: syntax error\n", 1);
+		return (0);
+	}
+	return (1);
+}
+
+int check_unlosed_quotes(char *command, int i)
+{
+    char quote;
+
+    while (command[i] && command[i] != '\'' && command[i] != '\"')
+        i++;
+    if (command[i] == '\0')
+        return 1;
+    quote = command[i++];
+    while (command[i] && command[i] != quote)
+        i++;
+    if (command[i] == '\0')
+        return (ft_putstr_fd("unclosed quote\n", 2), 0);
+    if (command[i] == quote && command[i + 1])
+        return check_unlosed_quotes(command, i + 1);
+    return (1);
 }
 
 int	is_valid_command(char *command)
@@ -61,46 +140,23 @@ int	is_valid_command(char *command)
 		i++;
 	if (command[i] == '\0')
 		return (0);
+	// if (!check_unlosed_quotes(&command[i], 0))
+	// 	return (0);
 	while (command[i])
 	{
-		if (command[i] == '\'' || command[i] == '\"')
-		{
-			char quote = command[i];
-			while (command[i] && command[i] != quote)
-				i++;
-			if (command[i] == quote)
-				i++;
-			if (command[i] == '\0')
-				return (1);			
-		}
 		if (ft_isspace(command[i]))
 		{
 			i++;
 			continue ;
 		}
-		if (command[i] == '>' || command[i] == '<')
-		{
-			if (check_symbol(command, command[i], &(i)) == -1)
-				flag_redirection = 1;
-		}
-		if (command[i] == '|')
-		{
-			if (ft_isspace(command[i]))
-			{
-				while (ft_isspace(command[i]))
-					i++;
-				if (command[i] == '|')
-					flag_redirection = 1;
-			}
-			else if (command[i + 1] == '|' || command[i + 1] == '\0')
-				flag_redirection = 1;
-		}
+		if (ft_check_quote(command, 34, &i) == -1 || ft_check_quote(command,39 ,&i) == -1)
+			return(0);
+		if (handle_redirection(&flag_redirection, command, &i) == 1)
+			return(ft_putstr_fd("bash: syntax error\n", 1), 0);
+		handle_pipe(&flag_redirection, command, &i);
 		i++;
 	}
-	if (flag_redirection == 1)
-	{
-		ft_putstr_fd("bash: syntax error\n", 1);
+	if (print_syntax_error(&flag_redirection) == 0)
 		return (0);
-	}
 	return (1);
 }
